be03ce5728f3565518dd7c89c7b7098a6417bd90
jerustes <j.erustes@gmail.com>
2018-01-29 19:19:59 +0100

    UPDATE login_linux.c
    Almost complete: resist buffer OF, passw aging and trial counter, no plain text storage, prevents online brute force (passw), protection against signal exiting

--- login_linux.c
+++ login_linux.c
@@ -11,30 +11,48 @@
 #include <pwd.h>
 #include <sys/types.h>
 #include <crypt.h>
-/* Uncomment next line in step 2 */
-/* #include "pwent.h" */
+#include "pwent.h"
 
 #define TRUE 1
 #define FALSE 0
 #define LENGTH 16
+#define HARD_L 10
+#define PENALTY 10
 
 void sighandler() {
+	//Signaling handling routines here
 
-	/* add signalhandling routines here */
-	/* see 'man 2 signal' */
+	//signal(SIGINT, SIG_IGN);
+	int i;
+	for(i = 1; i <=31 ; i++)
+	{
+	   signal(i,SIG_IGN);
+	}
+	/* Sigaction is recommended instead of signal
+		int sigaction(int signum, const struct sigaction *act,
+              struct sigaction *oldact);
+    */
+
 }
+
 
 int main(int argc, char *argv[]) {
 
-	struct passwd *passwddata; /* this has to be redefined in step 2 */
+	mypwent *passwddata;
 	/* see pwent.h */
 
 	char important[LENGTH] = "***IMPORTANT***";
 
 	char user[LENGTH];
-	//char   *c_pass; //you might want to use this variable later...
+	char *enc_pass; 		//Encrypted password 
 	char prompt[] = "password: ";
 	char *user_pass;
+
+	//Password age
+	int limit_age = 10;
+	//Failed attempts
+	int limit_attmp = 5;
+	int i_penalty = PENALTY; //penalty counter
 
 	sighandler();
 
@@ -47,30 +65,108 @@ int main(int argc, char *argv[]) {
 		fflush(NULL); /* Flush all  output buffers */
 		__fpurge(stdin); /* Purge any data in stdin buffer */
 
-		if (gets(user) == NULL) /* gets() is vulnerable to buffer */
-			exit(0); 							/*  overflow attacks.  */
+		/*
+		if (gets(user) == NULL) 
+			exit(0); 							
+		*/
+
+		if(fgets(user, sizeof(user), stdin) == NULL){ //user --> Name
+
+
+
+			printf("OVERFLOW");
+			exit(0);
+		}else{
+			/*
+			char aux = '\0';
+			strtok(user, '\n');
+			strcat(user, aux);
+			*/
+//@TODO polish
+			user[strlen(user)-1] = '\0';
+		}
+
 
 		/* check to see if important variable is intact after input of login name - do not remove */
 		printf("Value of variable 'important' after input of login name: %*.*s\n",
 				LENGTH - 1, LENGTH - 1, important);
 
 		user_pass = getpass(prompt);
-		passwddata = getpwnam(user);
+
+		if(mygetpwnam(user) == NULL){
+			//printf("WRONG USER\n");
+			continue;
+		}
+		passwddata = mygetpwnam(user); //mypwent struct
+		
 
 		if (passwddata != NULL) {
 			/* You have to encrypt user_pass for this to work */
 			/* Don't forget to include the salt */
 
-			if (!strcmp(user_pass, passwddata->pw_passwd)) {
+			/* We will assume db data is all correct and fitting requisites:
+				salt is two-digit correctly and the encryption is done with that one */
+
+			//CRYPT
+			enc_pass = crypt(user_pass, passwddata->passwd_salt);
+
+
+			if (!strcmp(enc_pass, passwddata->passwd)) { //CORRECT LOGIN
+
+				//reset password_failures and age +1
+				reset_failed(user, passwddata);
+				age_pass(user, passwddata);
+
+				if(passwddata->pwage >= limit_age){ //Password too old, alert!
+					printf("*WARNING* Password is too old!\n");
+				}
 
 				printf(" You're in !\n");
-
+				break;
 				/*  check UID, see setuid(2) */
 				/*  start a shell, use execve(2) */
 
 			}
 		}
+		//Failed attempt
+		failed_attmpt(user, passwddata);
 		printf("Login Incorrect \n");
+
+		//Brute force limits
+			//soft limit
+		if(passwddata->pwfailed >= limit_attmp && passwddata->pwfailed < HARD_L){ //Failed too much, give penalty!
+			i_penalty = PENALTY;
+			printf("Too many failed attempts");
+
+			while(i_penalty <= PENALTY && i_penalty > 0) {
+				printf("%d\n", i_penalty);
+					sleep(3);
+					i_penalty--;
+			}
+		}
+
+			//HARD limit
+		if(passwddata->pwfailed >= HARD_L){
+			printf("ATTACK DETECTED. Shutting down...\n");
+			sleep(1);
+			printf("...\n");
+			sleep(1);
+			printf("bye\n");
+			sleep(1);
+			exit(0);
+			//system("shutdown -P now");
+		}
+		/* All this, assuming the attacker doesn´t close the program and open it again,
+		   in which case, the soft limit "time penalty" would be avoided and
+		   the hard limit just kicks you out, so you could go in and try again
+			
+			possible solutions:
+		   HARD: block login even when using the correct password
+		*/
+
 	}
 	return 0;
 }
+
+
+
